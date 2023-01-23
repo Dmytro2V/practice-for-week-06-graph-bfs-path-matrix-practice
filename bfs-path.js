7f692818fdb5c1b890d7576458d20601096b0219
Dmytro2V <op2vin@gmail.com>
2023-01-23 08:16:33 +0200

    done

--- bfs-path.js
+++ bfs-path.js
@@ -1,20 +1,57 @@
 function findNeighbors(node, matrix) {
-    // Up
-
-    // Down
-
-    // Left
-
-    // Right
-
-    // Your code here
+     // Your code here
+     let nbors = []
+     let row = node[0];
+     let col = node[1]
+    
+     // Check top 
+     if (row > 0) nbors.push([row - 1, col]);  
+     // Check bottom
+     if (row < matrix.length - 1) nbors.push([row + 1, col]);  
+     // Check left
+     if (col > 0) nbors.push([row, col - 1]);  
+     // Check right
+     if (col < matrix[0].length - 1) nbors.push([row, col + 1])
+   
+     return nbors;
 }
 
 
 function bfsPath(matrix, startNode, endValue) {
     // Your code here
-}
+    let path = []; // for return
+    let queue = [startNode] // queueing node;
+    let visitedStr = new Set([nodeStr(startNode)]) // keep tracking in string format
 
+    while (queue.length > 0) { //traversing all or until find finish
+                              // this case we can also make 'while' 
+                              // on (new nbors) and no shift  
+      let currentNode = queue.shift();
+      path.push(currentNode); // store for output     
+      let row = currentNode[0];
+      let col = currentNode[1];
+      if (matrix[row][col] === endValue) return path; // check to finish
+
+      // else queue new neighbours
+      let nbors = findNeighbors(currentNode, matrix);
+      for (const nbor of nbors) {
+        let nborStr = nodeStr(nbor)
+        if (!visitedStr.has(nborStr)) {
+            visitedStr.add(nborStr);
+            queue.push(nbor);
+        }
+
+      }
+   
+    }
+    return false // if no target find    
+};
+
+function nodeStr(node) {
+    let row = node[0];
+    let col = node[1];
+    return `${row}, ${col}`;
+}
 
 // ***** UNCOMMENT FOR LOCAL TESTING *****
 
